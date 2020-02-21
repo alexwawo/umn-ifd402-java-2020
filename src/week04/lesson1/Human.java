2e9b43f57a3adefddd1d2749e643f25dbe8b655b
alexwawo <alex.wawo@gmail.com>
2020-02-21 10:15:05 +0700

    static modifier

--- src/week04/lesson1/Human.java
+++ src/week04/lesson1/Human.java
@@ -1,8 +1,12 @@
 package week04.lesson1;
 
 public class Human {
-    String hair_color, eye_color, name, gender;
-    int age, weight, height;
+    private String hair_color, eye_color, name, gender;
+    private static int age;
+    private int weight;
+    public int height;
+    private int lifespan;
+    public static int current_year = 2020;
 
     Human(int weight, int height) {
         this.height = height;
@@ -10,11 +14,19 @@ public class Human {
         this.age = 0;
     }
 
+    public String getName() {
+        return this.name;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
 
-    public void birthday() {
-        this.age++;
+    public static void birthday() {
+        addAge();
+    }
+
+    private static void addAge() {
+        age++;
     }
 }
--- src/week04/lesson1/MyAwesomeProgram.java
+++ src/week04/lesson1/MyAwesomeProgram.java
@@ -4,6 +4,18 @@ public class MyAwesomeProgram {
     public static void main(String[] args) {
         Human john = new Human(3, 50);
         john.setName("John Doe");
-        
+
+        Human jane = new Human(2, 40);
+        jane.setName("Jane Doe");
+
+        System.out.println(john.current_year);
+        System.out.println(jane.current_year);
+        System.out.println(Human.current_year);
+
+        john.current_year = 2025;
+
+        System.out.println(john.current_year);
+        System.out.println(jane.current_year);
+        System.out.println(Human.current_year);
     }
 }
