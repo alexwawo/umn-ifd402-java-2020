92ceb1bca1af7b5ee5e3b031564fc1bda2ed8402
alexwawo <alex.wawo@gmail.com>
2020-04-17 08:48:30 +0700

    week 10 more

--- src/week10/swing_gui/Swing1.java
+++ src/week10/swing_gui/Swing1.java
@@ -5,10 +5,10 @@ import javax.swing.*;
 public class Swing1 {
     public static void main(String[] args) {
         JFrame.setDefaultLookAndFeelDecorated(true);
-        JFrame container = new JFrame("Window Utama");
-        container.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        container.setSize(300, 100);
-        container.setLocation(200,200);
-        container.setVisible(true);
+        JFrame frame = new JFrame("Window Utama");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 100);
+        frame.setLocation(200,200);
+        frame.setVisible(true);
     }
 }
